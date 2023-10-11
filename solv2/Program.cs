ce84961c1838bf685e90ab892295d4fb2ffecb16
Anastasya <gbshell2022@gmail.com>
2023-10-11 15:00:33 +0300

    решение 2 задачи

--- solv2/Program.cs
+++ solv2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Random rend = new Random();
+// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+// 456->46
+//782 - >72
+// 918->98
+// Решение задачи
+Random rand = new Random();
+int randomNumber = rand.Next(100, 1000);
+int leftDigit = randomNumber / 100;
+int rightDigit = randomNumber % 10;
+int result = leftDigit * 10 + rightDigit;
+System.Console.WriteLine(randomNumber + "->"+ result);
+
