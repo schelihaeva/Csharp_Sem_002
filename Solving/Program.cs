5e6789cdd1f183e1e7d2a75264f3f6c6391af2cf
Anastasya <gbshell2022@gmail.com>
2023-10-11 14:00:40 +0300

    решение задачи1

--- Solving/Program.cs
+++ Solving/Program.cs
@@ -2,3 +2,19 @@
 // 78->8
 //12->2
 //85->8
+int randomNumber = new Random().Next(10,100); // в диапазоне в скобке слева число будет входить в расчет, а справа-не будет входить в расчет!!! Запомнить при такой функции!!
+System.Console.WriteLine(randomNumber);
+// 67/10 - >6 (получится 6) узнаем левую цифру числа
+// 67/% 10- > 7 (получаем остаток от деления 7) узнаем правую числу числа
+//
+ // int leftDigit = randomNumber / 10;
+//int rightDigit = randomNumber % 10;
+
+
+if (randomNumber / 10 > randomNumber % 10)
+{
+    System.Console.WriteLine(randomNumber / 10);
+}
+else{
+    System.Console.WriteLine(randomNumber % 10);
+}
