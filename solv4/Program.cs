3014fc3ce98de49424ac277bc93c43f2a672e9f8
Anastasya <gbshell2022@gmail.com>
2023-10-11 19:52:03 +0300

    данныее задачи4

--- solv4/Program.cs
+++ solv4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// наишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+// 14 -> нет
+// 46 - > нет
+// 161- > да
+
+System.Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number%7 == 0) $$ (number/23 ==0)){
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}
