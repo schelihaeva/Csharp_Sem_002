caeb09c0a21fb4940398b82dbaebfce9cd73ffc5
Anastasya <gbshell2022@gmail.com>
2023-10-11 20:13:47 +0300

    решение задачи5

--- solv5/Program.cs
+++ solv5/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на входе принимает два числа и проверяет, является ли одно число квадратом втрого
+//5,25->да
+//4,16 ->lf
+//8,9 ->нет
+
+System.Console.WriteLine("введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(secondNumber == firstNumber * firstNumber || firstNumber == secondNumber * secondNumber){
+    System.Console.WriteLine("yes");
+}
+else{
+    System.Console.WriteLine("no");
+}
