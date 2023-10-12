7c0c90814509827312381233d0df399da98dfc88
Anastasya <gbshell2022@gmail.com>
2023-10-12 09:54:23 +0300

    решение DZ_002

--- DZ_002/Program.cs
+++ DZ_002/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int Prompt(string message)
+{
+
+System.Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+int thirdNumber (int number){
+    while (number > 999){
+        number /=10;
+    }
+    return number % 10;
+} 
+bool NoNumber (int number){
+    if (number < 100){
+        System.Console.WriteLine("третье цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = Prompt ("Введите число >");
+if (NoNumber(number)){
+    System.Console.WriteLine(thirdNumber(number));
+}
