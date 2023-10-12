305b8966853783f08cfb2af1fbbc52412ed8a007
Anastasya <gbshell2022@gmail.com>
2023-10-12 09:39:24 +0300

    решение DZ_001

--- DZ_001/Program.cs
+++ DZ_001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int Prompt(string message)
+{
+
+System.Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите трехзначное число");
+if (number >=100 && number <1000)
+{
+int mid = number / 10 % 10;
+System.Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+}
+else{
+System.Console.WriteLine("Это число не трехзначное");
+}
